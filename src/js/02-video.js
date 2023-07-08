33e7dd44f4bb90079efc76bc8e19d41f3444858f
VlCore <korovin_vladislav@icloud.com>
2023-07-09 02:46:52 +0300

    vimeo error

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Vimeo from '@vimeo/player'
 
+import throttle from 'lodash.throttle'
+
+const iframe = document.querySelector('iframe')
+const player = new Vimeo('vimeo-player')
+const saveCurrentTime = throttle(function(currentTime) {
+    localStorage.setItem('"videoplayer-current-time"', currentTime)
+}, 1000)
+
+player.on('timeupdate', function(data) {
+    const currentTime = data.seconds
+    saveCurrentTime(currentTime)
+})
+
+window.addEventListener('DOMContentLoaded', function() {
+    const currentTime = this.localStorage.getItem('"videoplayer-current-time"')
+    if (currentTime) {
+        player.setCurrentTime(currentTime)
+    }
+})
